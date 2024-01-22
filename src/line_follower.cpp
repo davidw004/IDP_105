4cc7edf5c1f2a73b9153860533ddb9b1f2ecc182
davidw004 <daw99@cam.ac.uk>
2024-01-22 13:13:00 +0000

    added simple outline for moving functions

--- src/line_follower.cpp
+++ src/line_follower.cpp
@@ -1,9 +1,7 @@
 #include "Arduino.h"
 #include "Line_Follower.h"
-Line_Follower::Line_Follower(bool LineColor, bool A_D_Scan, uint8_t PWM_R_EN, uint8_t PWM_L_EN, uint8_t MAX_PWM_SPEED)
+Line_Follower::Line_Follower(uint8_t PWM_R_EN, uint8_t PWM_L_EN, uint8_t MAX_PWM_SPEED)
 {
-    _LineColor = LineColor;       // Black Line =LOW , White Line =HIGH;
-    _A_D_Scan = A_D_Scan;           // Analog Scan = LOW , Digital Scan = HIGH; 
     _PWM_R_EN = PWM_R_EN;
     _PWM_L_EN = PWM_L_EN;
     _MAX_PWM_SPEED = (255 * MAX_PWM_SPEED) / 100;
@@ -11,38 +9,52 @@ Line_Follower::Line_Follower(bool LineColor, bool A_D_Scan, uint8_t PWM_R_EN, ui
     pinMode(A1, INPUT);
     pinMode(A2, INPUT);
     pinMode(A3, INPUT);
-    pinMode(A4, INPUT);
     pinMode(_PWM_R_EN, OUTPUT);
     pinMode(_PWM_L_EN, OUTPUT);
 }
 
-void Line_Follower::Go() {
+void Line_Follower::go() {
+    //Read input from sensors (not yet figured out what values are being returned yet)
+    _A0 = map(analogRead(A0), 0, 850, 0, (_MAX_PWM_SPEED * 0.7));
+    _A1 = map(analogRead(A1), 0, 850, 0, (_MAX_PWM_SPEED * 0.3));
+    _A2 = map(analogRead(A2), 0, 850, 0, (_MAX_PWM_SPEED * 0.3));
+    _A3 = map(analogRead(A3), 0, 850, 0, (_MAX_PWM_SPEED * 0.7));
+    
+    //If both A1 A2 black keep driving at maxspeed
+    if (_A1 == 0 && _A2 == 0){
+        analogWrite(_PWM_L_EN, baseSpeedLeft);
+        analogWrite(_PWM_R_EN, baseSpeedRight);
+    }
+    else if (_A1 == 1 && _A2 == 0){ //If left high and right low then change motor speed to turn left
+
+    }
+
+    /*
     if (_A_D_Scan == LOW)   // Analog Scan = LOW  
     {
         _A0 = map(analogRead(A0), 0, 850, 0, (_MAX_PWM_SPEED * 0.7));
         _A1 = map(analogRead(A1), 0, 850, 0, (_MAX_PWM_SPEED * 0.3));
-        _A2 = map(analogRead(A2), 0, 850, 0, _MAX_PWM_SPEED);
-        _A3 = map(analogRead(A3), 0, 850, 0, (_MAX_PWM_SPEED * 0.3));
-        _A4 = map(analogRead(A4), 0, 850, 0, (_MAX_PWM_SPEED * 0.7));
+        _A2 = map(analogRead(A2), 0, 850, 0, (_MAX_PWM_SPEED * 0.3));
+        _A3 = map(analogRead(A3), 0, 850, 0, (_MAX_PWM_SPEED * 0.7));
 
-        _left_wieght = _A0 + _A1;
-        _right_wieght = _A3 + _A4;
-        _left_wieght = constrain(_left_wieght, 0, _MAX_PWM_SPEED);
-        _right_wieght = constrain(_right_wieght, 0, _MAX_PWM_SPEED);
+        _left_weight = _A0 + _A1;
+        _right_weight = _A3 + _A4;
+        _left_weight = constrain(_left_weight, 0, _MAX_PWM_SPEED);
+        _right_weight = constrain(_right_weight, 0, _MAX_PWM_SPEED);
 
         // for sharp corners 
-        if (_left_wieght > (_MAX_PWM_SPEED * 0.8)) _right_wieght = _right_wieght * 0.3;
-        if (_right_wieght > (_MAX_PWM_SPEED * 0.8)) _left_wieght = _left_wieght * 0.3;
+        if (_left_weight > (_MAX_PWM_SPEED * 0.8)) _right_weight = _right_weight * 0.3;
+        if (_right_weight > (_MAX_PWM_SPEED * 0.8)) _left_weight = _left_weight * 0.3;
 
         if (_LineColor == LOW) // Black Line =LOW 
         {
-            analogWrite(_PWM_R_EN, (_MAX_PWM_SPEED - _right_wieght));
-            analogWrite(_PWM_L_EN, (_MAX_PWM_SPEED - _left_wieght));
+            analogWrite(_PWM_R_EN, (_MAX_PWM_SPEED - _right_weight));
+            analogWrite(_PWM_L_EN, (_MAX_PWM_SPEED - _left_weight));
         }
         else                // White Line =HIGH;
         {
-            analogWrite(_PWM_R_EN, _right_wieght);
-            analogWrite(_PWM_L_EN, _left_wieght);
+            analogWrite(_PWM_R_EN, _right_weight);
+            analogWrite(_PWM_L_EN, _left_weight);
         }
     }
     else                     // Digital Scan = HIGH;
@@ -120,8 +132,9 @@ void Line_Follower::Go() {
             break;
         }
     }
+    */
 }
-void Line_Follower::Stop() {
+void Line_Follower::stop() {
     analogWrite(_PWM_R_EN, 0);
     analogWrite(_PWM_L_EN, 0);
 }
--- src/line_follower.h
+++ src/line_follower.h
@@ -2,22 +2,20 @@
 #include "Arduino.h"
 class Line_Follower {
 public:
-    Line_Follower(bool LineColor, bool A_D_Scan, uint8_t PWM_R_EN, uint8_t PWM_L_EN, uint8_t MAX_PWM_SPEED); // :
-    void  Go();
-    void  Stop();
-    void leftTurn();
+    Line_Follower(uint8_t PWM_R_EN, uint8_t PWM_L_EN, uint8_t MAX_PWM_SPEED); // :
+    void go();
+    void stop();
+
 private:
-    bool _LineColor;
-    bool _A_D_Scan;
     uint8_t _Position;
-    uint8_t _A0, _A1, _A2, _A3, _A4;
+    uint8_t _A0, _A1, _A2, _A3;
     uint8_t _R_Speed, _L_Speed;
     uint8_t _PWM_R_EN, _PWM_L_EN;
     uint8_t _MAX_PWM_SPEED;
-    uint8_t _left_wieght;
-    uint8_t _right_wieght;
-    uint8_t maxspeeda = 150;
-    uint8_t maxspeedb = 150;
-    uint8_t basespeeda = 100;
-    uint8_t basespeedb = 100;
+    uint8_t _left_weight;
+    uint8_t _right_weight;
+    uint8_t maxSpeedLeft = 150;
+    uint8_t maxSpeedRight = 150;
+    uint8_t baseSpeedLeft = 100;
+    uint8_t baseSpeedRight = 100;
 };
