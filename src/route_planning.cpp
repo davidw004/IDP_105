7339263f5f22130762cb9bf8ade3f2ee1b044d94
davidw004 <daw99@cam.ac.uk>
2024-01-24 10:47:50 +0000

    included route planning in line testing branch

---
+++ src/route_planning.cpp
@@ -0,0 +1,1 @@
+#include "route_planning.h"
---
+++ src/route_planning.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Arduino.h"
+
+enum Decision{STRAIGHT, LEFT, RIGHT};
+struct Routes{
+    //Block 1
+    Decision routeOne[2] = {LEFT,RIGHT};
+    Decision returnOneGreen[2] = {RIGHT, LEFT};
+    Decision returnOneRed[3] = {LEFT, STRAIGHT, RIGHT};
+
+    //Block 2
+    Decision routeTwoRed[3] = {STRAIGHT, LEFT, LEFT};
+    Decision routeTwoGreen[4] = {STRAIGHT, RIGHT, STRAIGHT, RIGHT};
+    Decision returnTwoGreen[4] = {LEFT, STRAIGHT, LEFT, STRAIGHT};
+    Decision returnTwoRed[3] = {RIGHT, RIGHT, STRAIGHT};
+};
