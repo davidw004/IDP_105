c6224c64d7fd469481a9b9f0a88b720597653444
Oliver Griffiths <olivergriffiths17@gmail.com>
2024-01-30 13:14:36 +0000

    Block collect
    block collect class

--- src/cube_retrieval.cpp
+++ src/cube_retrieval.cpp
@@ -1,1 +1,24 @@
 #include "cube_retrieval.h"
+#include "ArduinoPinout.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Cube_Retrieval::Cube_Retrieval()
+{
+}
+
+void Cube_Retrieval::setup(){
+
+
+}
+void Cube_Retrieval::collect(){
+    _clawMotor = AFMS.getMotor(3);
+    delay 2000;
+    _clawMotor -> setSpeed(closingSpeed);
+}
+void Cube_Retrieval::fetch1(){
+    /* go forward specifed distance by running line 
+    follower code for a set amount of time and calling void. 
+    Run written block collection code (written in this class)
+    before running line follower reverse code(needs to be 
+    written)*/
+}
--- src/cube_retrieval.h
+++ src/cube_retrieval.h
@@ -8,12 +8,26 @@
 class Cube_Retrieval 
 {
     public:
+        Cube_Retrieval();
+        void setup();
+        void collect();
+        void fetch1();
     //Constructor
     //Function for moving forward certain distance up to cube
     //Function for detecting type of cube and turning on light
     //Function for picking up cube
 
     private:
+        uint8_t _extremeLeftReading, _leftReading, _rightReading, _extremeRightReading;
+        uint8_t _left_weight;
+        uint8_t _right_weight;
+        uint8_t maxSpeedLeft = 200;
+        uint8_t maxSpeedRight = 200;
+        uint8_t baseSpeedLeft = 150;
+        uint8_t baseSpeedRight = 150;
+        uint8_t blocksCollected = 100;
+        uint8_t closingSpeed = 200;
+        Adafruit_DCMotor *_clawMotor;
     //member variables:
     //LEDs for cube detection
     //Motor for picking up cube
